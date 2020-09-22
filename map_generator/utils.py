3dcd9feab4b7bf81857f47996dfa7c8cd35d5308
Ronaldo Martins <ronaldo.rmsjr@gmail.com>
2020-09-22 15:07:35 -0300

    Ajustes exportar padrão bdgex

--- map_generator/utils.py
+++ map_generator/utils.py
@@ -56,7 +56,24 @@ class MapTools:
         json_file.close()
         return json_data
 
-            
+    def update_qpt_variables(self, composition, novo_valor, chave='edition_folder'):        
+        if 'variableNames' in composition.customProperties():
+            chaves = composition.customProperty('variableNames')
+            valores = composition.customProperty('variableValues')
+            index_of_key = -1
+            if chave in chaves:
+                index_of_key = chaves.index(chave)
+                valores[index_of_key] = novo_valor
+                composition.setCustomProperty('variableValues', valores)
+            else:
+                chaves.append(chave)
+                valores.append(novo_valor)
+                composition.setCustomProperty('variableNames', chaves)
+                composition.setCustomProperty('variableValues', valores)
+        else:
+            composition.setCustomProperty('variableNames', [chave] )
+            composition.setCustomProperty('variableValues', [novo_valor])
+
     def selectEpsg(self, hemisferio, fuso):		
         self.epsg_selected = True				
         self.hemisferio = hemisferio
@@ -145,25 +162,62 @@ class MapTools:
             self.iface.messageBar().pushMessage("Layer de moldura não selecionado.", text_message, level=Qgis.Info, duration=3)
             return False
 
-    def exportMap(self, composition):        			
+    def exportMap(self, composition):        
         basename = self.mi + '_' + self.inom 
         pdf_filename = os.path.join(self.saveFolder,basename + ".pdf") 
         tiff_filename = os.path.join(self.saveFolder, basename + ".tif")
         exporter = QgsLayoutExporter(composition)        
         exportPdf = True
+        
         if exportPdf:
             pdf_settings = QgsLayoutExporter.PdfExportSettings()            
             pdf_settings.rasterizeWholeImage = True
             pdfExport_result = exporter.exportToPdf(pdf_filename, pdf_settings)				
         if self.dlg.checkBox_exportar_geotiff.isChecked():
-            imageExport_result = exporter.exportToImage(tiff_filename, QgsLayoutExporter.ImageExportSettings())		
+            imageExport_result = exporter.exportToImage(tiff_filename, QgsLayoutExporter.ImageExportSettings())		        
+        del exporter
+        # QgsProject.instance().setCrs(QgsCoordinateReferenceSystem(int(self.epsg),QgsCoordinateReferenceSystem.EpsgCrsId))
 
     def removeCreationDataGroups(self, map_groups = ['localizacao' , 'articulacao', 'map','minimap',  'divisao', 'localizacao_nome_estado']):
         root = QgsProject.instance().layerTreeRoot()
         for group in map_groups:
             group_localizacao = root.findGroup(group)
             root.removeChildNode(group_localizacao)
-            
+
+    def reproject(self, origin_path, target_path, source_epsg , target_epsg=4674):
+        source_path = " -of GTiff " + origin_path				            
+        sc_target_epsg = " -t_srs EPSG:" + str(target_epsg)
+        #cmd = "gdalwarp  -co COMPRESS=LZW -overwrite -s_srs EPSG:" + \        
+        cmd = "gdalwarp  -co COMPRESS=JPEG -co \"TILED=YES\" -overwrite -s_srs EPSG:" + \
+        str(source_epsg) + \
+        sc_target_epsg + \
+        source_path + \
+        " " + \
+        target_path										
+        os.system(cmd)
+        
+    def remove_alpha_band(self, origin_path, target_path):
+        #cmd = "gdal_translate -b 1 -b 2 -b 3 -co COMPRESS=JPEG " + \
+        cmd = "gdal_translate -b 1 -b 2 -b 3 -co COMPRESS=JPEG -co \"TILED=YES\"  " + \
+        origin_path + \
+        " " + \
+        target_path										
+        os.system(cmd)
+
+    def convert_ycbcr(self, origin_path, target_path):            
+        cmd = "gdal_translate -co COMPRESS=JPEG -co \"TILED=YES\" -co PHOTOMETRIC=YCBCR " + \
+        origin_path + \
+        " " + \
+        target_path										
+        os.system(cmd)
+
+    def convert(self, origin_path, target_path):
+        cmd = "gdal_translate  -co PHOTOMETRIC=YCBCR  " + \
+        origin_path + \
+        " " + \
+        target_path										
+        os.system(cmd)
+
     def reprojectTiffs(self):
         folder_reprojetado = os.path.join(self.saveFolder, 'padrao_bdgex')
         os.mkdir(folder_reprojetado)
@@ -178,22 +232,16 @@ class MapTools:
                 del rlayer
 
                 rasterSource = rasterPath
-                # rasterSource = provider.dataSourceUri()
-
-                source_path = " -of GTiff " + rasterSource				
                 filename_without_ext = (file.split('.'))[0]
+                caminho_reprojetado = os.path.join(folder_reprojetado,filename_without_ext + '_reprojetado.tif') 
+                self.reproject(rasterPath, caminho_reprojetado, source_epsg ,4674)
+                caminho_rgb = os.path.join(folder_reprojetado, filename_without_ext + '_rgb.tif') 
+                self.remove_alpha_band(rasterPath, caminho_rgb)
+
                 target_path  = os.path.join(folder_reprojetado, filename_without_ext + '.tif') 
-
-                new_espg = 4674
-                target_epsg = " -t_srs EPSG:" + str(new_espg)
-
-                cmd = "gdalwarp  -co COMPRESS=LZW -overwrite -s_srs EPSG:" + \
-                str(source_epsg) + \
-                target_epsg + \
-                source_path + \
-                " " + \
-                target_path										
-                os.system(cmd)
+                self.convert_ycbcr(caminho_rgb, target_path)
+                os.remove(caminho_reprojetado)
+                os.remove(caminho_rgb)
 
     def createTifPdfFolders(self, saveFolder):        
             os.mkdir(saveFolder)
