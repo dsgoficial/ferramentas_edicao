5e80b103942946c9a44923e5281a579042a04ad3
Ronaldo Martins <ronaldo.rmsjr@gmail.com>
2020-09-24 14:20:06 -0300

    Changes for output checking

--- manager/default_map.py
+++ manager/default_map.py
@@ -73,8 +73,8 @@ product_parameters = {
 			"llcolor" : QColor('black'),
 			"linwidth_geo" : 0.3,
 			"linwidth_utm" : 0.2,
-			"linwidth_buffer_geo" : 0.1,
-			"linwidth_buffer_utm" : 0.1,
+			"linwidth_buffer_geo" : 0,
+			"linwidth_buffer_utm" : 0,
 			"geo_grid_color"			: QColor('white'),
 			"utm_grid_color"			: QColor('black'),
 			"geo_grid_buffer_color" : QColor('white'),
@@ -83,7 +83,13 @@ product_parameters = {
 	},
 	'carta_ortoimagem_tipo_i':{
 		"nome_produto":"Carta Ortoimagem Tipo I",
-		"caminho_layout":"/includes/produtos/carta_ortoimagem_tipo_I/carta_ortoimagem_tipo_I.qml",
+		"required_files":[
+			['map_generator','limites','estados_2019.shp'],
+			['map_generator','limites','internacional.shp'],
+			['map_generator','limites','municipios_2019.shp'],
+			['map_generator','limites','oceano.shp'],
+			['map_generator','limites','paises.shp']
+		],
 		"qpt":{
 			"25":{
 				"projeto":{
@@ -130,7 +136,7 @@ product_parameters = {
 			"250":{
 				"projeto":{
 					"x_0":7,
-					"y_0":487,
+					"y_0":395,
 					"width":110,
 					"height":70,				
 				},
@@ -154,12 +160,12 @@ product_parameters = {
 			"llcolor" : QColor('black'),
 			"linwidth_geo" : 0.3,
 			"linwidth_utm" : 0.2,
-			"linwidth_buffer_geo" : 0,
-			"linwidth_buffer_utm" : 0,
+			"linwidth_buffer_geo" : 0.1,
+			"linwidth_buffer_utm" : 0.1,
 			"geo_grid_color"			: QColor('black'),
 			"utm_grid_color"			: QColor('black'),
-			"geo_grid_buffer_color" : QColor('black'),
-			"utm_grid_buffer_color" : QColor('black')
+			"geo_grid_buffer_color" : QColor('white'),
+			"utm_grid_buffer_color" : QColor('white')
 		}
 	}
 }
@@ -173,15 +179,21 @@ class DefaultMap(MapManager):
 		self.epsg_selected 	= False
 		self.scale_selected = False
 		self.utm_grid 		= UtmGrid()
-		self.setProductParameters(product_parameters)		
+		self.set_products_parameters(product_parameters)		
 	
 	def setProdutoConfig(self):
+		success = True
+		return_param = None
 		# Define a pasta onde serao salvos os arquivos de saida
 		caminho_pasta_exportar 	= self.dlg.mQgsFileWidget_pasta_exportar.splitFilePaths(self.dlg.mQgsFileWidget_pasta_exportar.filePath())
 		if len(caminho_pasta_exportar)>0:
-			self.baseSaveFolder = caminho_pasta_exportar[0]
+			if os.path.isdir(caminho_pasta_exportar[0]):
+				self.baseSaveFolder = caminho_pasta_exportar[0]
+			else:
+				success = False
 		else:
 			self.baseSaveFolder = os.path.join(os.path.dirname(os.path.dirname(__file__)),'includes', 'output')
+		return success, return_param
 
 	def editCompositions(self, tipo_produto, dict_compositions):
 		for escala, compositor in dict_compositions.items():			
@@ -199,14 +211,19 @@ class DefaultMap(MapManager):
 				list_dict_qpts.append(dict_cabecalho)		
 			self.htmlData.editQpts(compositor, list_dict_qpts)
 	
-	def setCartaConfig(self, path_json_carta, connectedUri,  dict_compositions):
+	def setCartaConfig(self, path_json_carta, connectedUri,  dict_compositions, teste_no_layers=False):
 		# Obtendo o dict do caminho json	
 		if os.path.exists(path_json_carta):
 			dict_carta = self.readJsonFromPath(path_json_carta)
 
 			# inom 
 			inomen = dict_carta['inom']
+			mi 		= self.utm_grid.get_MI_MIR_from_inom(inomen)
 			escala = str(self.utm_grid.getScale(inomen))
+
+			# Composition Map Item     
+			self.changeProjectVariable(escala,mi)
+			print('Project Variable Changed - ' + str(mi))
 
 			# Tipo de produto
 			str_tipo_produto = self.dlg.comboBox_tipo_produto.currentText()
@@ -218,6 +235,8 @@ class DefaultMap(MapManager):
 			
 			# Camadas para o produto
 			path_json_produto = os.path.join(os.path.dirname(os.path.dirname(__file__)),'map_generator', 'produtos', tipo_produto, 'camadas.json')
+			if teste_no_layers:
+				path_json_produto = os.path.join(os.path.dirname(os.path.dirname(__file__)),'map_generator', 'produtos', tipo_produto, 'camadas.json')
 			dict_camadas_produto = self.readJsonFromPath(path_json_produto)
 
 			# Maptables e Minimaptables
@@ -268,20 +287,23 @@ class DefaultMap(MapManager):
 
 			}
 
-			return True, inom, layers
+			return True, composition, inom, layers
 		else:
-			return False , None, None
+			return False , None, None, None
 		
 	def createCompositions(self, list_of_scales, tipo_produto):
-		dict_compositions = {}
+		dict_compositions = {}		
+		novo_valor = os.path.join(os.path.dirname(__file__),'..','map_generator','produtos', tipo_produto)
 		for scale in list(set(list_of_scales)):
 			if scale == '250':			
 				caminho_layout = os.path.join(os.path.dirname(__file__),'..','map_generator','produtos', tipo_produto, tipo_produto + '_250' +'.qpt')		
-				composition = self.MapC.getPrintLayoutFromQptPath(caminho_layout)		
+				composition = self.MapC.getPrintLayoutFromQptPath(caminho_layout, novo_valor)					
+				composition.refresh()
 				dict_compositions['250'] = composition
 			else:
 				caminho_layout = os.path.join(os.path.dirname(__file__),'..','map_generator','produtos', tipo_produto, tipo_produto +'.qpt')		
-				composition = self.MapC.getPrintLayoutFromQptPath(caminho_layout)		
+				composition = self.MapC.getPrintLayoutFromQptPath(caminho_layout, novo_valor)		
+				composition.refresh()
 				dict_compositions[scale] = composition
 		return dict_compositions				
 
@@ -335,23 +357,27 @@ class DefaultMap(MapManager):
 				logs.append(error)
 		return success, logs, list_of_scales
 
-
-
 	def createMaps(self):
-		# Set project crs
-		my_crs = QgsCoordinateReferenceSystem(4326,QgsCoordinateReferenceSystem.EpsgCrsId)
-		QgsProject.instance().setCrs(my_crs)
+		test_noLayers = True
+		showLayers = True
 
-		oldProjValue = self.mc.setProjectProjection()
+		# Set project crs		
+		oldProjValue = self.mc.setProjectProjection()		
+		
 		# Cria as instancias do mapa
-		self.createMapInstances()
-
-		self.setProdutoConfig()
+		self.create_map_instances()
 
 		# Obtem o tipo de produto selecionado na Ui
 		str_tipo_produto = self.dlg.comboBox_tipo_produto.currentText()
 		tipo_produto = '_'.join(str_tipo_produto.lower().split(' '))
-
+		
+		success, return_param = self.setProdutoConfig()		
+		
+		if showLayers:						
+			manager = QgsProject.instance().layoutManager()
+			if manager.layoutByName(str_tipo_produto):
+				manager.removeLayout(manager.layoutByName(str_tipo_produto))
+		
 		# Obtendo os arquivos de carta da Ui
 		caminhos_json_carta 	= self.dlg.mQgsFileWidget_json_cartas.splitFilePaths(self.dlg.mQgsFileWidget_json_cartas.filePath())
 		
@@ -361,27 +387,39 @@ class DefaultMap(MapManager):
 		
 		# Edit composition with project and credits qpt
 		dict_compositions = self.createCompositions(list_of_scales, tipo_produto)
-		self.editCompositions(tipo_produto, dict_compositions)
+		self.editCompositions(tipo_produto, dict_compositions)		
 		
 		if success:
-			success_connection, connectedUri = self.getFirstConnection( caminhos_json_carta)
-			
-			self.saveFolder = os.path.join(self.baseSaveFolder, dt.now().strftime('%Y_%m_%d_%H_%M_%S'))
-			os.mkdir(self.saveFolder)
+			success_connection, connectedUri = self.getFirstConnection( caminhos_json_carta)			
+			if success_connection:
+				self.saveFolder = os.path.join(self.baseSaveFolder, dt.now().strftime('%Y_%m_%d_%H_%M_%S'))
+				os.mkdir(self.saveFolder)
 
-			logs = []
-			for caminho_json_carta in caminhos_json_carta:
-				if self.checkJsonCarta(caminho_json_carta):
-					# Set config for html labels
-					success, inomen, layers = self.setCartaConfig(caminho_json_carta, connectedUri,  dict_compositions)										
-					# Get feature data for maps
-					self.getDefaultFeatureData(inomen)	
-					
-					# Create grid layer
-					grid_layer, grid_layerId, feature_inom = self.createGridLayer(inomen)
-					QgsProject.instance().addMapLayer(grid_layer, False)
+				logs = []
+				for caminho_json_carta in caminhos_json_carta:
+					if self.checkJsonCarta(caminho_json_carta):
+						# Set config for html labels					
+						success, composition, inomen, layers = self.setCartaConfig(caminho_json_carta, connectedUri,  dict_compositions, test_noLayers)															
+						
+						# Get feature data for maps
+						int_escala, mi = self.getDefaultFeatureData(inomen)							
+						self.changeProjectVariable(str(int_escala), mi)
+						
+						# Create grid layer
+						grid_layer, grid_layerId, feature_inom = self.createGridLayer(inomen)
+						QgsProject.instance().addMapLayer(grid_layer, False)
 
-					self.setElementsConfig(tipo_produto)
-					composition = dict_compositions[str(self.utm_grid.getScale(inomen))]
-					self.createAll(composition, self.nome, feature_inom, grid_layer, layers)
-		self.mc.setProjectProjection(oldProjValue)
+						self.setElementsConfig(tipo_produto)
+						# composition = dict_compositions[str(self.utm_grid.getScale(inomen))]						
+						self.createAll(composition, self.nome, feature_inom, grid_layer, layers, showLayers)
+						if showLayers:						
+							manager = QgsProject.instance().layoutManager()						
+							composition.setName(str_tipo_produto)
+							manager.addLayout(composition)
+							break
+				# Reprojeta se for o caso		
+				if self.dlg.checkBox_exportar_geotiff.isChecked():
+					self.reprojectTiffs()
+		if not showLayers:
+			self.mc.setProjectProjection(oldProjValue)
+			pass
