a7e35be1b1b0676d6b4da54a0cd996ee5535fa4b
Eliton <elitonfilho@gmail.com>
2021-08-24 16:00:37 -0300

    feat: ordering legend

--- map_generator/elements/legend.py
+++ map_generator/elements/legend.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from collections import OrderedDict
 
 from PyQt5.QtXml import QDomDocument
 from PyQt5.QtCore import QPoint
@@ -7,7 +8,43 @@ from qgis.core import QgsReadWriteContext
 
 class Legend():
 
-    def __init__(self) -> None:
+    order_group = {
+        'sistemas_de_transporte': {
+            'order': 0,
+            'order_classes': ['rodovia_pavimentada', 'prefixo_rodovia_federal', 'rodovia_nao_pavimentada', 'prefixo_rodovia_estadual',
+            'ferrovia_bitola_metrica', 'ferrovia_bitola_internacional_larga', 'pista_pouso', 'heliponto', 'atracadouro_terminal']
+        }
+        ,
+        'hidrografia': {
+            'order': 1,
+            'order_classes': ['massa_dagua', 'trecho_drenagem', 'terreno_sujeito_inundacao', 'queda_dagua_corredeira']
+        },
+        'energia_e_comunicacoes': {
+            'order': 2,
+            'order_classes': ['subestacao_energia_eletrica', 'trecho_de_energia']
+        },
+        'estrutura_economica': {
+            'order': 3,
+            'order_classes': ['extracao_mineral']
+        },
+        'limites_e_localidades': {
+            'order': 4,
+            'order_classes': ['capital_federal_simbolo', 'capital_federal_texto', 'capital_estadual_simbolo', 'capital_estadual_texto', 
+            'cidade_simbolo', 'cidade_texto','vila_simbolo', 'vila_texto', 'aglomerado_rural', 'nome_local', 'area_militar', 'terra_indigena', 
+            'unidade_de_conservacao', 'limite_estadual', 'limite_internacional']
+        },
+        'relevo': {
+            'order': 5,
+            'order_classes': ['curva_nivel_mestra', 'curva_nivel_depressao', 'curva_nivel_normal', 'ponto_cotado', 'curva_nivel_auxiliar',
+            'elemento_fisiografico']
+        },
+        'classes_auxiliares': {
+            'order': 6,
+            'order_classes': ['obstaculo_vertical', 'area_sem_dados']
+        }
+    }
+
+    def __init__(self):
         _p = Path(__file__).parent.parent / 'produtos' / 'carta_ortoimagem' 
         self.qptsPath = _p / 'symbols' / 'legend'
         with open(_p / 'mapClassToLegend.json') as mappingData:
@@ -54,6 +91,13 @@ class Legend():
             else:
                 groupedClasses.update({group:set(labels)})
         return groupedClasses
+
+    def orderLegend(self, legend):
+        orderedLegend = OrderedDict()
+        for group in sorted(legend, key=lambda x: self.order_group.get(x).get('order')):
+            classes = legend.get(group)
+            orderedLegend.update({group: sorted(classes, key=lambda x: self.order_group.get(group).get('order_classes').index(x))})
+        return orderedLegend
         
     def loadQDomComponent(self, path):
         doc = QDomDocument()
@@ -65,5 +109,6 @@ class Legend():
         self.legendMappingData = self.legendMappingData.get(str(scale))
         legendClassesToDisplay = defaults.orthoMandatoryClasses.union(defaults.orthoOptionalClasses.intersection(set(cfg)))
         legendClassesGrouped = self.groupLegend(legendClassesToDisplay)
-        self.buildLegend(composition, legendClassesGrouped)
+        legendClassesOrdered = self.orderLegend(legendClassesGrouped)
+        self.buildLegend(composition, legendClassesOrdered)
             
