557dc603b409734779c99b3cc3971b70648628ac
Eliton <elitonfilho@gmail.com>
2021-09-17 11:09:13 -0300

    fix: set miniMap crs

--- map_generator/elements/minimap.py
+++ map_generator/elements/minimap.py
@@ -1,52 +1,40 @@
-from qgis.core import QgsProject, QgsLayerTreeGroup
-from PyQt5.QtGui import * 
+from qgis.core import QgsLayerTreeGroup, QgsProject, QgsCoordinateReferenceSystem
+
 from .map_utils import MapParent
 
+
 class MiniMap(MapParent):
-	def __init__(self):		
-		self.layers_to_show = []
 
-	def setParams(self):
-		pass
-
-
-	def make(self, composition, selected_feature, layers, showLayers = False):	
+	def make(self, composition, mapArea, layers, showLayers=False):	
 		self.deleteGroups(['minimap'])	
 
 		root = QgsProject.instance().layerTreeRoot()		
-		miniMapGroup_node = QgsLayerTreeGroup('minimap')	
-		miniMapGroup_node.setItemVisibilityChecked(False)								
+		miniMapGroupNode = QgsLayerTreeGroup('minimap')	
+		miniMapGroupNode.setItemVisibilityChecked(False)								
 		
-		map_extent = selected_feature.geometry().convexHull().boundingBox()
+		mapExtents = mapArea.geometry().convexHull().boundingBox()
 		
 		# Creating layers to lock map
-		layers_to_lock = []
-		layers_to_lock.extend(layers['minimap'])
-		layers_to_lock.extend(layers['images'])
-		self.updateMapItem(composition, map_extent, layers_to_lock)	
+		layersToLock = []
+		layersToLock.extend(layers['minimap'])
+		layersToLock.extend(layers['images'])
+		self.updateMapItem(composition, mapExtents, layersToLock)	
 
 		if showLayers:
-			for map_layer in layers_to_lock:
-				miniMapGroup_node.addLayer(map_layer)
+			for layer in layersToLock:
+				miniMapGroupNode.addLayer(layer)
 			root = QgsProject.instance().layerTreeRoot()		
-			root.addChildNode(miniMapGroup_node)
+			root.addChildNode(miniMapGroupNode)
 
 		return layers['id_minimap']
-		
-		#  quando as camadas forem passadas como parametro
-		# miniMapGroup_node.setItemVisibilityChecked(False)
-		# QgsProject.instance().addMapLayer(camadaAdicionada, False)
-		# miniMapGroup_node.addLayer(camadaAdicionada)
-		# root = QgsProject.instance().layerTreeRoot()		
-		# root.addChildNode(miniMapGroup_node)
     
-	def updateMapItem(self, composition, map_extent, layers_to_lock, mapItem=None):    	
-		if mapItem is None:
-			mapItem = composition.itemById("map_miniMap")
-		if mapItem is not None:
+	@staticmethod
+	def updateMapItem(composition, mapExtents, layersToLock, mapItem=None):    	
+		if (mapItem:=composition.itemById("map_miniMap")) is not None:
 			mapSize = mapItem.sizeWithUnits()
-			mapItem.setFixedSize(mapSize)			
-			if layers_to_lock is not None:		 	
-				mapItem.setLayers(layers_to_lock)
-			mapItem.setExtent(map_extent)		
+			mapItem.setFixedSize(mapSize)
+			if layersToLock is not None:
+				mapItem.setLayers(layersToLock)
+			mapItem.setExtent(mapExtents)
+			mapItem.setCrs(QgsCoordinateReferenceSystem('EPSG:4674'))
 			mapItem.refresh()
