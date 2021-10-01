b3d5569a09fdafeb8b64edd111359bf42eb6a201
Eliton <elitonfilho@gmail.com>
2021-10-01 16:06:30 -0300

    refactor: organizing map.py

--- map_generator/elements/map.py
+++ map_generator/elements/map.py
@@ -1,80 +1,53 @@
-import os
 from pathlib import Path
-from PyQt5.QtGui import *
+
 from PyQt5.QtCore import QVariant
-from qgis.core import (
-    QgsVectorLayer, QgsField, QgsCoordinateReferenceSystem, QgsProject,
-    QgsCoordinateTransform, QgsFeature, QgsLayerTreeGroup, QgsLayoutSize,
-    QgsUnitTypes, QgsUnitTypes, QgsLayoutItem
-)
-from qgis import processing
+from PyQt5.QtGui import *
+from qgis.core import (QgsCoordinateReferenceSystem, QgsCoordinateTransform,
+                       QgsFeature, QgsField, QgsLayerTreeGroup, QgsLayoutItem,
+                       QgsLayoutSize, QgsProject, QgsUnitTypes, QgsVectorLayer)
 
 from .map_utils import MapParent
+
 
 class Map(MapParent):
     def __init__(self, GLC):
         self.GLC = GLC
-        self.folder_estilos = Path(__file__).parent.parent / 'styles' / 'map'
+        self.stylesFolder = Path(__file__).parent.parent / 'styles' / 'map'
 
-    def setMapSize(self, map_width=588, map_height=588):
-        self.map_width = map_width
-        self.map_height = map_height
+    def setMapSize(self, mapWidth=588, mapHeight=588):
+        self.mapWidth = mapWidth
+        self.mapHeight = mapHeight
 
     def getExtent(self, feat):
         return feat.geometry().boundingBox()
 
-    # A feição criada irá servir de base para o grid
-    def createLayerForGrid(self, grid_layer, selected_feature):
-        # Create memory layer
-        uri_shape = "Polygon"
-        baseuri = uri_shape + "?crs=epsg:"
-        crs_epsg = '4326'
-        uri = baseuri + self.epsg
-        layer_auxiliarMoldura = QgsVectorLayer(uri, 'auxiliar_moldura', "memory")
-
-        # Start editing memory layer, set all grid fields to memory layer
-        layer_auxiliarMoldura.startEditing()
-        pr = layer_auxiliarMoldura.dataProvider()
-        fieldnames_gridlayer = [field.name() for field in grid_layer.fields()]
-        pr.addAttributes(grid_layer.fields())
-
-        # Cria id caso nao exista na camada de moldura
-        if 'id' not in fieldnames_gridlayer:
-            pr.addAttributes([QgsField("id",  QVariant.String)])
-        layer_auxiliarMoldura.updateFields()
-
-        # Adiciona uma feição com o mesmo contorno da moldura
-        geom = selected_feature.geometry()
-        convexhull = geom.convexHull()
-        crsSrc = QgsCoordinateReferenceSystem('EPSG:4326')    # WGS 84
-        self.crs_moldura = QgsCoordinateReferenceSystem(f'EPSG:{self.epsg}')  # WGS 84 / UTM zone 33N
-        self.geom_transformation = QgsCoordinateTransform(
-            crsSrc, self.crs_moldura, QgsProject.instance())
-        convexhull.transform(self.geom_transformation)
-
-        # Tenho que remover o boundTest
-        boundingBox_transformed = convexhull.boundingBox()
-
-        # Adiciono a feição do contorno
-        fet = QgsFeature(layer_auxiliarMoldura.fields())
-        fet.setGeometry(convexhull)
-        fet['id'] = '1'
+    def createLayerForGrid(self, baseLayer, mapExtents):
+        ''' Creates a vector layer that will be the base for the grid.
         '''
-        fet_attributes = selected_feature.attributes()
-        
-        # coloco como id um caso não tenha id 
-        idx = layer_auxiliarMoldura.fields().indexFromName('id')
-        if 'id' not in fieldnames_gridlayer:
-            idx = -1			
-        fet_attributes[idx] = '1'			
-        fet.setAttributes(fet_attributes)
-        '''
-        pr.addFeatures([fet])
+        uri = f'Polygon?crs=EPSG:{self.epsg}'
+        gridLayer = QgsVectorLayer(uri, 'auxiliar_moldura', "memory")
+        gridLayerDataProvider = gridLayer.dataProvider()
+        gridLayer.startEditing()
+        gridLayerFields = (field.name() for field in baseLayer.fields())
+        gridLayerDataProvider.addAttributes(baseLayer.fields())
 
-        # Commit changes
-        layer_auxiliarMoldura.commitChanges()
+        if 'id' not in gridLayerFields:
+            gridLayerDataProvider.addAttributes([QgsField("id",  QVariant.String)])
+        gridLayer.updateFields()
 
-        return layer_auxiliarMoldura, boundingBox_transformed
+        convexhull = mapExtents.geometry().convexHull()
+        crsSrc = QgsCoordinateReferenceSystem('EPSG:4326')
+        crcDest = QgsCoordinateReferenceSystem(f'EPSG:{self.epsg}')
+        transform = QgsCoordinateTransform(crsSrc, crcDest, QgsProject.instance())
+        convexhull.transform(transform)
+
+        feat = QgsFeature(gridLayer.fields())
+        feat.setGeometry(convexhull)
+        feat['id'] = '1'
+        gridLayerDataProvider.addFeature(feat)
+        gridLayer.commitChanges()
+
+        return gridLayer, convexhull.boundingBox()
 
     def setSpacingFromScale(self, scale):
         self.id_attr = 'id'
@@ -99,7 +72,7 @@ class Map(MapParent):
         self.utm_grid_buffer_color = utm_grid_buffer_color
         self.masks_check = masks_check
 
-    def generateGridStyleForLayer(self, layer_auxiliarMoldura):
+    def applyStyleGridLayer(self, layer_auxiliarMoldura):
         layer = layer_auxiliarMoldura
 
         layer_bound = layer
@@ -109,111 +82,97 @@ class Map(MapParent):
         feature_bound = [feat for feat in layer_auxiliarMoldura.getFeatures()][0]
         feature_geometry = feature_bound.geometry()
 
-        # self.GLC.geo_test(feature_geometry, layer_bound, utmSRID, self.id_attr, self.grid_id_value, self.spacing, self.crossX, self.crossY, self.scale, self.color, self.fontSize, self.font, self.fontLL, self.llcolor, self.linwidth_geo, self.linwidth_utm)
         self.GLC.styleCreator(feature_geometry, layer_bound, utmSRID, self.id_attr, self.id_value, self.spacing, self.crossX, self.crossY, self.scale, self.fontSize, self.font, self.fontLL, self.llcolor,
                               self.linwidth_geo, self.linwidth_utm, self.linwidth_buffer_geo, self.linwidth_buffer_utm, self.geo_grid_color, self.utm_grid_color, self.geo_grid_buffer_color, self.utm_grid_buffer_color, self.masks_check)
 
-    def make(self, composition, grid_layer, selected_feature, layers, showLayers=False):
-        # Deletendo o grupo
-        map_layers = []
+    def make(self, composition, mapExtentsLayer, mapExtents, layers, showLayers=False):
+
+        mapLayers = []
         self.deleteGroups(['map'])
 
-        # Cria o grupo map
-        root = QgsProject.instance().layerTreeRoot()
-        mapGroup_node = QgsLayerTreeGroup('map')
-        mapGroup_node.setItemVisibilityChecked(False)
+        instance = QgsProject.instance()
+        mapGroupNode = QgsLayerTreeGroup('map')
+        mapGroupNode.setItemVisibilityChecked(False)
 
-        # Criando o layer do grid e estilizando
-        map_extent = self.getExtent(selected_feature)
-        layer_grid_styled, map_extent_transformed = self.createLayerForGrid(
-            grid_layer, selected_feature)
-        QgsProject.instance().addMapLayer(layer_grid_styled, False)
-        copy = self.cloneVectorLayer(layer_grid_styled, 'aux_moldura')
-        # copyLabel = self.cloneVectorLayer(layer_grid_styled, 'aux_label')
+        # Setting up grid layer
+        mapExtent = self.getExtent(mapExtents)
+        gridLayer, mapExtentsTransformed = self.createLayerForGrid(mapExtentsLayer, mapExtents)
+        instance.addMapLayer(gridLayer, False)
+
+        # Setting up aux_label, which is reprojected to mapLayers
         crs = next(iter(layers['map'])).crs()
-        copyLabel = self.cloneVectorLayerReproject(layer_grid_styled, 'aux_label', crs)
+        copyLabel = self.cloneVectorLayerReproject(gridLayer, 'aux_label', crs)
         copyLabelStyle = Path(__file__).parent.parent / 'styles' / 'grid' / 'aux_label.qml'
         copyLabel.loadNamedStyle(str(copyLabelStyle))
-        QgsProject.instance().addMapLayer(copyLabel, False)
-        # copy = layer_grid_styled.clone()
+        instance.addMapLayer(copyLabel, False)
+
+        # Setting up aux_moldura
+        copy = self.cloneVectorLayer(gridLayer, 'aux_moldura')
         copyStyle = Path(__file__).parent.parent / 'styles' / 'grid' / 'style.qml'
         copy.loadNamedStyle(str(copyStyle))
-        QgsProject.instance().addMapLayer(copy, False)
-        map_layers.extend([copy.id(), copyLabel.id(), layer_grid_styled.id()])
-        # map_layers.append(copy.id())
-        # map_layers.append(copyLabel.id())
-        # map_layers.append(layer_grid_styled.id())
-        mapGroup_node.addLayer(layer_grid_styled)
-        self.generateGridStyleForLayer(layer_grid_styled)
-        layer_grid_styled.triggerRepaint()
+        instance.addMapLayer(copy, False)
 
-        # Criando o layer de mascara de rotulos
-        layer_mascara_rotulo = self.createMaskLayer(selected_feature)
-        QgsProject.instance().addMapLayer(layer_mascara_rotulo, False)
-        map_layers.append(layer_mascara_rotulo.id())
+        mapGroupNode.addLayer(gridLayer)
+        self.applyStyleGridLayer(gridLayer)
+        gridLayer.triggerRepaint()
 
+        # Setting up mask layer
+        maskLayer = self.createMaskLayer(mapExtents)
+        instance.addMapLayer(maskLayer, False)
 
-        # layers_to_lock = [layer_mascara_rotulo, copy, copyStyle, *layers['map'], *layers['images']]
-        layers_to_lock = [layer_mascara_rotulo]
-        layers_to_lock.extend([copy])
-        layers_to_lock.extend([copyLabel])
-        layers_to_lock.extend(layers['map'])
-        layers_to_lock.extend(layers['images'])
-        self.updateMapItem2(composition, map_extent, map_extent_transformed,
-                            layer_grid_styled, layers_to_lock)
-        map_layers.extend(layers['id_map'])
+        mapLayers.extend((copy.id(), copyLabel.id(), gridLayer.id(), maskLayer.id(), *layers['id_map']))
+        layersToComposition = [maskLayer, copy, copyLabel, *layers['map'], *layers['images']]
+        self.updateMapItem(composition, mapExtent, mapExtentsTransformed, gridLayer, layersToComposition)
 
         if showLayers:
-            for map_layer in layers_to_lock:
-                mapGroup_node.addLayer(map_layer)
-
+            for lyr in layersToComposition:
+                mapGroupNode.addLayer(lyr)
             root = QgsProject.instance().layerTreeRoot()
-            root.addChildNode(mapGroup_node)
+            root.addChildNode(mapGroupNode)
 
-        return map_layers
+        return mapLayers
 
-    def createMaskLayer(self, selected_feature):
-        bound = selected_feature.geometry()
-        buffered_bound = bound.buffer(0.3, 2)
-        dif_bound = buffered_bound.difference(bound)
-        layer_mascara_rotulo = self.createVectorLayerFromIter('mascara_rotulo', [dif_bound])
-        caminho_estilo_mascara = os.path.join(self.folder_estilos, 'mascara_rotulos.qml')
-        layer_mascara_rotulo.loadNamedStyle(caminho_estilo_mascara)
-        layer_mascara_rotulo.triggerRepaint()
-        return layer_mascara_rotulo
-        # QgsProject.instance().addMapLayer(grid_rectangle_layer, True)
+    def createMaskLayer(self, mapExtents):
+        bound = mapExtents.geometry()
+        bufferedBound = bound.buffer(0.3, 2)
+        diffBound = bufferedBound.difference(bound)
+        maskLayer = self.createVectorLayerFromIter('mascara_rotulo', [diffBound])
+        maskLayer.loadNamedStyle(str(self.stylesFolder / 'mascara_rotulos.qml'))
+        maskLayer.triggerRepaint()
+        return maskLayer
 
-    def updateMapItem2(self, composition, map_extent, map_extent_transformed, layer_auxiliarMoldura, layers_to_lock):
+    def updateMapItem(self, composition, mapExtents, mapExtentsTransformed, frameLayer, layersToComposition):
+        '''Updates the composer
+        '''
         theScale = self.scale*1000.0
         mapItem = composition.itemById("the_map")
-        mapItem.setExtent(map_extent)
+        mapItem.setExtent(mapExtents)
         mapItem.setScale(theScale)
-        if layers_to_lock is not None:
-            layers_to_set = [layer_auxiliarMoldura]
-            layers_to_set.extend(layers_to_lock)
-            mapItem.setLayers(layers_to_set)
+        if layersToComposition is not None:
+            layersToSet = [frameLayer, *layersToComposition]
+            mapItem.setLayers(layersToSet)
         else:
-            mapItem.setLayers([layer_auxiliarMoldura])
+            mapItem.setLayers([frameLayer])
         mapItem.refresh()
-        mapItem.setCrs(self.crs_moldura)
-        mapItem.setExtent(map_extent_transformed)
+        mapItem.setCrs(QgsCoordinateReferenceSystem(f'EPSG:{self.epsg}'))
+        mapItem.setExtent(mapExtentsTransformed)
         mapItem.setScale(theScale)
         if theScale == 250000:
-            self.map_height = 494
-            self.map_width = 724
+            self.mapHeight = 494
+            self.mapWidth = 724
         mapItem.attemptResize(QgsLayoutSize(
-            self.map_width,  self.map_height, QgsUnitTypes.LayoutMillimeters))
+            self.mapWidth,  self.mapHeight, QgsUnitTypes.LayoutMillimeters))
         mapItem.setScale(theScale)
         mapItem.refresh()
         self.centerMapInAreaCarta(composition)
 
     def centerMapInAreaCarta(self, composition):
-        item_area_reservada_carta = composition.itemById('area_reservada_carta')
-        if item_area_reservada_carta is not None:
-            item_area_reservada_carta.setReferencePoint(QgsLayoutItem.Middle)
-            item_area_reservada_carta.refresh()
-            position_area_reservada_carta = item_area_reservada_carta.positionWithUnits()
-
-            item_the_map = composition.itemById('the_map')
-            item_the_map.setReferencePoint(QgsLayoutItem.Middle)
-            item_the_map.attemptMove(position_area_reservada_carta)
+        ''' Sets reference point for the_map and area_reservada_carta elements
+        '''
+        if (mapReservedArea:=composition.itemById('area_reservada_carta')) is not None:
+            mapReservedArea.setReferencePoint(QgsLayoutItem.Middle)
+            mapReservedArea.refresh()
+            positionMapReservedArea = mapReservedArea.positionWithUnits()
+            mapItem = composition.itemById('the_map')
+            mapItem.setReferencePoint(QgsLayoutItem.Middle)
+            mapItem.attemptMove(positionMapReservedArea)
