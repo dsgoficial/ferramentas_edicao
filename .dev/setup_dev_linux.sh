eba22e9175747131e3757f192f7c1e55a5a4d30c
phborba <philipeborba@gmail.com>
2023-03-13 22:22:54 -0300

    adding dev helpers

---
+++ .dev/setup_dev_linux.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ln -s ${PWD}/.. ~/.local/share/QGIS/QGIS3/profiles/default/python/plugins/ferramentas_edicao
---
+++ .dev/setup_dev_macos.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ln -s ${PWD}/.. ~/Library/Application\ Support/QGIS/QGIS3/profiles/default/python/plugins/ferramentas_edicao
